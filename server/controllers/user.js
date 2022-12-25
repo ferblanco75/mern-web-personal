791d6fe16010970e349cb2d8b71b604247c368e0
fernando <ferblanco@gmail.com>
2022-12-25 17:13:41 -0300

    agrego funcionalidad y endpoints de crear usuario y de obtener usuarios

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,61 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const { use } = require("../router/user");
+
+
+//GET ME me trae datos del usuario qeu le paso el token por header,
+//tiene que ser un usuario activo y logueado
 async function getMe(req,res){
-    res.status(200).send({msg: "ok"});
+    const { user_id } = req.user;
+
+    const response = await User.findById(user_id);
+
+    if (!response) {
+        res.status(400).send({ msg: "no se encuentra el usuario buscado" })
+    } else {
+        res.status(200).send({ msg: "todo ok" });
+    }
 }
 
-module.exports = {
-    getMe,
+//VOY A HACER UN ENDPOINT QUE ME TRAE TODOS LOS USUARIOS 
+//se va a llamer getUsers  
+//luego de creado lo armo en el router
+async function getUsers(req,res){
+        const { active } = req.query;
+        let response = null;
+        if (active === undefined){
+            response = await User.find();
+        } else {
+            response = await User.find( {active});
+        }
+        //console.log(response);
+        res.status(200).send(response);
 }
+
+async function createUser(req,res){
+    const { password } = req.body;
+    const user = new User({ ...req.body, active: false });
+    //encripta el password con el salt que le da bcrypt
+    const salt = bcrypt.genSaltSync(10);
+    const hasPassword = bcrypt.hashSync(password,salt);
+    user.password = hasPassword;
+
+    if(req.files.avatar){
+        //TODO : completar la funcion de procesar
+        console.log("procesar avatar");
+    }
+    user.save((error, userStore)=> {
+        if(error){
+            res.status(500).send({ msg: "error al crear, no se guardo el usuario"})
+        } else {
+            res.status(201).send( { msg: "usuario creado ok"})
+        }
+    } )
+}
+
+
+
+
+module.exports = {
+    getMe, getUsers,createUser,
+};  
--- server/middleware/authenticated.js
+++ server/middleware/authenticated.js
@@ -4,18 +4,20 @@ function asureAuth(req,res,next){
     if(!req.headers.authorization) {
         res.status(403).send({msg: "la peticion no tiene cabecera de autenticacion"})
     }
+
+    const token = req.headers.authorization.replace("Bearer ", "");
+
     try {
-        const payload = jwt.decoded;
+        const payload = jwt.decoded(token);
 
         const { exp } = payload;
         const  currentData = new Date().getTime();
-        console.log(currentData);
-        console.log(exp);
 
         if (exp <= currentData ){
             return res.status(400).send({msg: "El token ha expirado"});
         }
-        req.user(payload);
+
+        req.user = payload;
         next(); 
     } catch (error) {
         return res.status(400).send({msg: "token invalido"})
